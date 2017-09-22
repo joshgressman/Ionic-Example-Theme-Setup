be137246c63fbaf418a2bc288294dd0f4a69d16c
joshgressman <joshgressman@gmail.com>
2017-09-22 16:38:30 -0500

    added custom themes

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  isWindows: Boolean;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public platform: Platform) {
+   this.isWindows = platform.is('windows');
   }
 
 }
